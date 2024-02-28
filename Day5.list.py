201c670a9e44792e2db3f6cd9fa931affaa31d19
ankithaT27 <104780117+ankithaT27@users.noreply.github.com>
2024-02-28 12:36:53 +0530

    Create Day5.list.py

---
+++ Day5.list.py
@@ -0,0 +1,20 @@
+list=[2,5,22,1,980,43,7]
+
+print(list)
+#sort
+list.sort()
+print(list)
+#addelements->adds at last
+list.append(5)
+print(list)
+#insert@someposition
+list.insert(0,55)
+print(list)
+list.remove(7)
+print(list)
+print(list[0])
+#printarange
+print(list[0:2])
+list.extend([9,77,64])
+print(list)
+print(max(list))
