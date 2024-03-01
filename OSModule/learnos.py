cac1fc14d2976964c372e043872ec9a858fc523f
ankithaT27 <104780117+ankithaT27@users.noreply.github.com>
2024-03-01 18:53:51 +0530

    Update learnos.py

--- OSModule/learnos.py
+++ OSModule/learnos.py
@@ -1,1 +1,21 @@
+#prints pwd
+print("The present dir is", os.getcwd())
+a=os.getcwd()
+print(a)
 
+#change dir
+os.chdir("/workspaces")
+
+#check if we are in a aprticular dir
+if a=="/workspaces":
+    print("yes you are")
+else:
+    print("no")
+
+#change to a dir 
+if a=="/workspaces":
+    print("yes, right")
+else:
+    os.chdir("/workspaces")
+    print("now we are in workspaces")
+    print(os.getcwd())
